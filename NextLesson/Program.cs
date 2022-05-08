4a4a2d2b3db6119c528b7a2fed2c6872bacfb08c
Victor Botvinko <rusty.vik@gmail.com>
2022-05-08 19:56:04 +0300

    modified

--- NextLesson/Program.cs
+++ NextLesson/Program.cs
@@ -1,5 +1,7 @@
 ﻿using NextLesson;
 
+CPerson [] array = new CPerson [6];
+
 CPerson firstPerson = new();
 
 firstPerson.LastName = "Botvinko";
@@ -8,52 +10,49 @@ firstPerson.MiddleName = "Alekseevich";
 
 CPerson secondPerson = new();
 secondPerson.LastName = "Botvinko";
-secondPerson.Name = "Victoria";
-secondPerson.MiddleName = "Yur'evna";
+secondPerson.Name = "VictorRR";
+secondPerson.MiddleName = "AlekseevichCHCH";
 
 
 CStudent firstStudent = new();
-firstStudent.LastName = "Golubev";
+firstStudent.LastName = "Botvinko";
 firstStudent.Name = "Nikita";
-firstStudent.MiddleName = "Nikolaevich";
+firstStudent.MiddleName = "Alekseevich";
 firstStudent.Average = 7.8;
 
 
 CStudent secondStudent = new();
-secondStudent.LastName = "Grakovich";
+secondStudent.LastName = "Botvinko";
 secondStudent.Name = "Elizaveta";
-secondStudent.MiddleName = "Dmitrievna";
+secondStudent.MiddleName = "Alekseevich";
 secondStudent.Average = 8.9;
 
 
 CProfessor firstProfessor = new();
-firstProfessor.LastName = "Gorbach";
+firstProfessor.LastName = "Botvinko";
 firstProfessor.Name = "Pavel";
-firstProfessor.MiddleName = "Victorovich";
+firstProfessor.MiddleName = "Alekseevich";
 firstProfessor.Publications = 5;
 firstProfessor.Position = "Docent";
 firstProfessor.Age = 35;
 
 
 CProfessor secondProfessor = new();
-secondProfessor.LastName = "Osipchik";
+secondProfessor.LastName = "Botvinko";
 secondProfessor.Name = "Aleksey";
-secondProfessor.MiddleName = "Dmitrievich";
+secondProfessor.MiddleName = "Alekseevich";
 secondProfessor.Publications = 3;
 secondProfessor.Position = "Prepodovatel";
 secondProfessor.Age = 29;
 
+array[0] = firstPerson;
+array [1] = secondPerson;
+array [2] = firstStudent;
+array [3] = secondStudent;
+array [4] = firstProfessor;
+array [5] = secondProfessor;
 
-firstPerson.ShowData();
-
-secondPerson.ShowData();
-
-firstStudent.ShowData();
-
-secondStudent.ShowData();
-
-firstProfessor.ShowData();
-
-secondProfessor.ShowData();
+for (int i = 0; i < array.Length; i++)
+{ array[i].ShowData(); }
 
 
